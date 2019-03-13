5d290f29b4e7f99fc05273cdafbed4d23e0bdbdd
kyou <wgoodq@foxmail.com>
2019-03-13 08:38:21 +0800

    优化先进先出队列实现

--- life_game_ui.py
+++ life_game_ui.py
@@ -42,7 +42,6 @@ def main():
 
     # 演化生命网格
     lf = LifeGrid()
-
     lb.bind('<Button-1>', handlerAdaptor(label_click, window=window, text=text, lf=lf))
 
     center_window(window)
@@ -162,7 +161,7 @@ class LifeGrid:
     def update_grid(self):
 
         # 保存网格历史状态，以便分析生命网格是否存现平衡（循环的稳定状态）
-        self._history.add(self._gridNew)
+        self._history.add(self._grid)
 
         # 逐个推算当前单元格的生存状态
         for r in range(len(self._grid)):
@@ -189,27 +188,18 @@ class LifeGrid:
 class FifoQueue:
     """先进先出、定长队列"""
 
-    def __init__(self, size=10, maxHead=10):
+    def __init__(self, size=10):
         """队列初始化、默认长度：10"""
-        self._queue = []
+        self._queue = [0 for i in range(size)]
         self._head = 0
         self._size = size
-        self._maxHead = maxHead
 
     def add(self, item):
-        self._queue.append(copy.deepcopy(item))
-        if len(self._queue) > self._size:
-            self._head += 1
-            self.clean()
-
-    def clean(self):
-        """避免队列过长或频繁截断数组"""
-        if self._head > self._maxHead:
-            self._queue = self._queue[self._head:]
-            self._head = 0
+        self._queue[self._head] = item
+        self._head = (self._head + 1) % self._size
 
     def __contains__(self, item):
-        return item in self._queue[self._head:]
+        return item in self._queue
 
     def get_queue(self):
         return self._queue
